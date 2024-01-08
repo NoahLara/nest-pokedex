95f9f2710800117bed0e6de846459da39476ebc7
nlara <noe_lara99@outlook.com>
2024-01-08 15:29:15 -0600

    feat: :sparkles: Global Prefix API
    - Adding set global prexi to API to affect all controllers in application

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Adding a global prefix for our API, this will affect all our application in all controllers automatically
+  app.setGlobalPrefix('api/v2');
   await app.listen(3000);
 }
 bootstrap();
