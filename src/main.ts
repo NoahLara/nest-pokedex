681ee1221d982f301b41d0b6f94eaa43428afce5
nlara <noe_lara99@outlook.com>
2024-01-10 23:02:30 -0600

    docs: :sparkles: Validations Pipes
    - Adding class-validator to project -
     Adding pipes validators in POST request for Pokemon controller
     - Adding Global Pipe validation and custom properties
     - Updating Pokemon DTO
     - Adding barrel exportation to Pokemon DTO's

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // Adding a global prefix for our API, this will affect all our application in all controllers automatically
+  // .:|Adding a global prefix for our API, this will affect all our application in all controllers automatically|:.
   app.setGlobalPrefix('api/v2');
+
+  // .:|Adding global validation endpoints|:.
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+
   await app.listen(3000);
 }
 bootstrap();
